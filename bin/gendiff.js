59e70cdb8467b99472c2fd1041e49f0e2b5f369f
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-06-02 22:47:10 +0400

    Add --format option to CLI with commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    // options.format доступен здесь, если понадобится
     const diff = genDiff(filepath1, filepath2);
     console.log(diff);
   });
