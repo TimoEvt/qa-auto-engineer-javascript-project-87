8d3ae0093c9a0af4d5c10ef23f9e94c98674eaf0
TimoEvt <t.evteev@school1387.ru>
2025-07-24 20:10:41 +0400

    genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -14,14 +14,18 @@ const genDiff = (filepath1, filepath2) => {
     const val2 = data2[key];
 
     if (!Object.hasOwn(data1, key)) {
+      // ключ есть только во втором файле — добавлен
       return `  + ${key}: ${val2}`;
     }
     if (!Object.hasOwn(data2, key)) {
+      // ключ есть только в первом — удалён
       return `  - ${key}: ${val1}`;
     }
     if (_.isEqual(val1, val2)) {
+      // значение не изменилось
       return `    ${key}: ${val1}`;
     }
+    // значение изменилось
     return `  - ${key}: ${val1}\n  + ${key}: ${val2}`;
   });
 
