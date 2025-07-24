bfcda21d71ced376d24d5d52884f53d4903fa162
TimoEvt <t.evteev@school1387.ru>
2025-07-24 19:51:42 +0400

    genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,10 +1,31 @@
-import parseFile from './parser.js';
+import fs from 'fs'
+import _ from 'lodash'
+
+const readJson = (filepath) => JSON.parse(fs.readFileSync(filepath, 'utf-8'))
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
-  return `File 1 keys: ${Object.keys(data1).join(', ')}\nFile 2 keys: ${Object.keys(data2).join(', ')}`;
-};
+  const data1 = readJson(filepath1)
+  const data2 = readJson(filepath2)
 
-export default genDiff;
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
 
+  const lines = keys.map((key) => {
+    const val1 = data1[key]
+    const val2 = data2[key]
+
+    if (!(key in data1)) {
+      return `  + ${key}: ${val2}`
+    }
+    if (!(key in data2)) {
+      return `  - ${key}: ${val1}`
+    }
+    if (_.isEqual(val1, val2)) {
+      return `    ${key}: ${val1}`
+    }
+    return `  - ${key}: ${val1}\n  + ${key}: ${val2}`
+  })
+
+  return `{\n${lines.join('\n')}\n}`
+}
+
+export default genDiff
