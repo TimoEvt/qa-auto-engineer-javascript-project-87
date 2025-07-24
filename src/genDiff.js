e92bb287d773aa6e504a4a30a4cb75f768eaa68a
TimoEvt <t.evteev@school1387.ru>
2025-07-24 20:17:30 +0400

    genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,11 +1,9 @@
-import fs from 'fs';
+import parseFile from './parser.js';
 import _ from 'lodash';
 
-const readJson = (filepath) => JSON.parse(fs.readFileSync(filepath, 'utf-8'));
-
 const genDiff = (filepath1, filepath2) => {
-  const data1 = readJson(filepath1);
-  const data2 = readJson(filepath2);
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
 
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
@@ -14,14 +12,18 @@ const genDiff = (filepath1, filepath2) => {
     const val2 = data2[key];
 
     if (!Object.hasOwn(data1, key)) {
+      // ключ есть только во втором файле — добавлен
       return `  + ${key}: ${val2}`;
     }
     if (!Object.hasOwn(data2, key)) {
+      // ключ есть только в первом — удалён
       return `  - ${key}: ${val1}`;
     }
     if (_.isEqual(val1, val2)) {
+      // значение не изменилось
       return `    ${key}: ${val1}`;
     }
+    // значение изменилось
     return `  - ${key}: ${val1}\n  + ${key}: ${val2}`;
   });
 
