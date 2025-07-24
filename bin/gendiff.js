285abef0611600c3bbc627af1d9e813bbef8dc30
TimoEvt <t.evteev@school1387.ru>
2025-07-24 19:47:56 +0400

    gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-import genDiff from '../src/genDiff.js';
+import genDiff from '../src/genDiff.js'
 
-const [,, filepath1, filepath2] = process.argv;
+const [,, filepath1, filepath2] = process.argv
 
 if (!filepath1 || !filepath2) {
-  console.error('Please provide two file paths');
-  process.exit(1);
+  console.error('Please provide two file paths')
+  process.exit(1)
 }
 
-console.log(genDiff(filepath1, filepath2));
-
+console.log(genDiff(filepath1, filepath2))
