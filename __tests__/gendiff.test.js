d99664350aab61f337064a231f6c7a4edf60bc73
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-09-25 18:53:10 +0400

    Resolve merge conflicts and apply local changes

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,5 @@
+test('sum test', () => {
+  const sum = (a, b) => a + b
+  expect(sum(2, 3)).toBe(5)
+})
+
--- src/genDiff.js
+++ src/genDiff.js
@@ -19,10 +19,38 @@ const genDiff = (filepath1, filepath2) => {
   const lines = keys.flatMap((key) => {
     const in1 = Object.hasOwn(obj1, key)
     const in2 = Object.hasOwn(obj2, key)
+
+import fs from 'node:fs';
+import path from 'node:path';
+import _ from 'lodash';
+
+// функция для парсинга JSON
+const parseFile = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  const ext = path.extname(fullPath);
+  const data = fs.readFileSync(fullPath, 'utf-8');
+
+  if (ext === '.json') return JSON.parse(data);
+
+  throw new Error(`Unsupported file format: ${ext}`);
+};
+
+// основная функция genDiff
+const genDiff = (filepath1, filepath2) => {
+  const obj1 = parseFile(filepath1);
+  const obj2 = parseFile(filepath2);
+
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+
+  const lines = keys.flatMap((key) => {
+    const in1 = Object.hasOwn(obj1, key);
+    const in2 = Object.hasOwn(obj2, key);
+
     if (in1 && in2) {
       if (_.isEqual(obj1[key], obj2[key])) return `    ${key}: ${obj1[key]}`
       return [`  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`]
     }
+
     if (in1) return `  - ${key}: ${obj1[key]}`
     if (in2) return `  + ${key}: ${obj2[key]}`
     return []
@@ -32,3 +60,16 @@ const genDiff = (filepath1, filepath2) => {
 }
 
 export default genDiff
+
+    if (in1) return `  - ${key}: ${obj1[key]}`;
+    if (in2) return `  + ${key}: ${obj2[key]}`;
+
+    return [];
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
+};
+
+export default genDiff;
+
+>>>>>>> 5314ec8 (Сохраняю текущие изменения перед pull)
