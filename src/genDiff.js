7b74eb22486e0ebb1c21b5e7dfe7f10e138b9643
TimoEvt <t.evteev@school1387.ru>
2025-09-25 19:16:27 +0400

    genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -20,31 +20,31 @@ const genDiff = (filepath1, filepath2) => {
     const in1 = Object.hasOwn(obj1, key)
     const in2 = Object.hasOwn(obj2, key)
 
-import fs from 'node:fs';
-import path from 'node:path';
-import _ from 'lodash';
+import fs from 'node:fs'
+import path from 'node:path'
+import _ from 'lodash'
 
 // функция для парсинга JSON
 const parseFile = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
-  const ext = path.extname(fullPath);
-  const data = fs.readFileSync(fullPath, 'utf-8');
+  const fullPath = path.resolve(process.cwd(), filepath)
+  const ext = path.extname(fullPath)
+  const data = fs.readFileSync(fullPath, 'utf-8')
 
-  if (ext === '.json') return JSON.parse(data);
+  if (ext === '.json') return JSON.parse(data)
 
-  throw new Error(`Unsupported file format: ${ext}`);
+  throw new Error(`Unsupported file format: ${ext}`)
 };
 
 // основная функция genDiff
 const genDiff = (filepath1, filepath2) => {
-  const obj1 = parseFile(filepath1);
-  const obj2 = parseFile(filepath2);
+  const obj1 = parseFile(filepath1)
+  const obj2 = parseFile(filepath2)
 
-  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
 
   const lines = keys.flatMap((key) => {
-    const in1 = Object.hasOwn(obj1, key);
-    const in2 = Object.hasOwn(obj2, key);
+    const in1 = Object.hasOwn(obj1, key)
+    const in2 = Object.hasOwn(obj2, key)
 
     if (in1 && in2) {
       if (_.isEqual(obj1[key], obj2[key])) return `    ${key}: ${obj1[key]}`
@@ -61,15 +61,13 @@ const genDiff = (filepath1, filepath2) => {
 
 export default genDiff
 
-    if (in1) return `  - ${key}: ${obj1[key]}`;
-    if (in2) return `  + ${key}: ${obj2[key]}`;
+    if (in1) return `  - ${key}: ${obj1[key]}`
+    if (in2) return `  + ${key}: ${obj2[key]}`
 
-    return [];
-  });
+    return []
+  })
 
-  return `{\n${lines.join('\n')}\n}`;
-};
+  return `{\n${lines.join('\n')}\n}`
+}
 
-export default genDiff;
-
->>>>>>> 5314ec8 (Сохраняю текущие изменения перед pull)
+export default genDiff
