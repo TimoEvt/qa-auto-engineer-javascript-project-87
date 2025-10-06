c0fc8a328c7da10dab29fca086f25dd355b1ace4
TimoEvt <t.evteev@school1387.ru>
2025-10-06 18:32:39 +0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 const getFileContent = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
-  return fs.readFileSync(fullPath, 'utf-8');
-};
+  const fullPath = path.resolve(process.cwd(), filepath)
+  return fs.readFileSync(fullPath, 'utf-8')
+}
 
-export default getFileContent;
-
+export default getFileContent
