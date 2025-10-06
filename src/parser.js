6e3f5feedc3fc8323b44f038e53b06887425bd7f
TimoEvt <t.evteev@school1387.ru>
2025-10-06 18:31:57 +0400

    parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,10 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 const parseFile = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
-  const data = fs.readFileSync(fullPath, 'utf-8');
-  return JSON.parse(data);
-};
+  const fullPath = path.resolve(process.cwd(), filepath)
+  const data = fs.readFileSync(fullPath, 'utf-8')
+  return JSON.parse(data)
+}
 
-export default parseFile;
-
+export default parseFile
