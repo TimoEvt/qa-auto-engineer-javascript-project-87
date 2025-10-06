2186841121f6eb79682f5fec79f8637db08903ff
TimoEvt <t.evteev@school1387.ru>
2025-10-06 20:56:17 +0400

    gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,23 @@
+import fs from 'fs'
+import path from 'path'
+import _ from 'lodash'
+import yaml from 'js-yaml'
+import formatters from './formatters/index.js'
+
+const parse = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath)
+  const ext = path.extname(fullPath).toLowerCase()
+  const data = fs.readFileSync(fullPath, 'utf-8')
+
+  if (ext === '.json') return JSON.parse(data)
+  if (ext === '.yml' || ext === '.yaml') return yaml.load(data)
+  throw new Error(`Unsupported file format: ${ext}`)
+}
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = parse(filepath1)
   const obj2 = parse(filepath2)
+
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
 
   const diff = keys.map((key) => {
@@ -19,3 +36,5 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   if (format === 'json') return JSON.stringify(diff, null, 2)
   return formatters[format](diff)
 }
+
+export default genDiff
