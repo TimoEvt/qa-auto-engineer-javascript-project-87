da3818b13f741257969f9f52dda360bc20bd7579
TimoEvt <t.evteev@school1387.ru>
2025-10-29 13:15:34 +0400

    Refactor stylish formatter to use lodash and improve output

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,20 +1,37 @@
-const stylish = (diff) => {
-  const lines = diff.map((item) => {
-    const { key, type, value, oldValue, newValue } = item
-    switch (type) {
+import _ from 'lodash'
+
+const indent = (depth) => ' '.repeat(depth * 4)
+
+const stringify = (value, depth) => {
+  if (_.isObject(value)) {
+    const lines = Object.entries(value).map(
+      ([key, val]) => `${indent(depth + 1)}${key}: ${stringify(val, depth + 1)}`
+    );
+    return `{\n${lines.join('\n')}\n${indent(depth)}}`
+  }
+  return String(value)
+}
+
+const stylish = (diff, depth = 0) => {
+  const lines = diff.map((node) => {
+    const currentIndent = indent(depth)
+    switch (node.type) {
       case 'added':
-        return `Property '${key}' was added with value: ${JSON.stringify(value)}`
+        return `${currentIndent}+ ${node.key}: ${stringify(node.value, depth + 1)}`
       case 'removed':
-        return `Property '${key}' was removed`
-      case 'updated':
-        return `Property '${key}' was updated. From ${JSON.stringify(oldValue)} to ${JSON.stringify(newValue)}`
+        return `${currentIndent}- ${node.key}: ${stringify(node.value, depth + 1)}`
       case 'unchanged':
-        return ''
+        return `${currentIndent}  ${node.key}: ${stringify(node.value, depth + 1)}`
+      case 'updated':
+        return [
+          `${currentIndent}- ${node.key}: ${stringify(node.oldValue, depth + 1)}`,
+          `${currentIndent}+ ${node.key}: ${stringify(node.newValue, depth + 1)}`
+        ].join('\n')
       default:
-        return ''
+        throw new Error(`Unknown type: ${node.type}`)
     }
-  }).filter(Boolean)
-  return lines.join('\n')
+  })
+  return `{\n${lines.join('\n')}\n${indent(depth)}}`
 }
 
 export default stylish
