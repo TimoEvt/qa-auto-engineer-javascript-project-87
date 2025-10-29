03f3ea81fd7698037be3e125352489f5559eb6f2
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-10-29 13:33:29 +0400

    Fix CLI parsing to handle -f option and pass format to genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,18 +2,30 @@
 
 import genDiff from '../src/genDiff.js'
 
-const [,, file1, file2] = process.argv
+const [,, ...args] = process.argv
+
+let format = 'stylish'
+let files = []
+
+args.forEach((arg, i) => {
+  if (arg === '-f') {
+    format = args[i + 1]
+  } else if (i === 0 || args[i - 1] !== '-f') {
+    files.push(arg)
+  }
+})
+
+const [file1, file2] = files
 
 if (!file1 || !file2) {
-  console.error('Usage: gendiff file1 file2')
+  console.error('Usage: gendiff [-f format] file1 file2')
   process.exit(1)
 }
 
 try {
-  const result = genDiff(file1, file2, 'json')
+  const result = genDiff(file1, file2, format)
   console.log(result)
 } catch (err) {
   console.error('Error:', err.message)
   process.exit(1)
 }
-
