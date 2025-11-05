a2de9c1e14207e4c2f46adabbb591c68c07f3f04
TimoEvt <t.evteev@school1387.ru>
2025-11-05 13:02:57 +0400

    Fix indentation in stylish.js formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,11 +1,11 @@
 import _ from 'lodash'
 
-const indent = (depth) => ' '.repeat(depth * 4 - 2)
+const indent = depth => ' '.repeat(depth * 4 - 2)
 
 const stringify = (value, depth) => {
   if (_.isObject(value)) {
     const lines = Object.entries(value).map(
-      ([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`
+      ([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`,
     )
     return `{\n${lines.join('\n')}\n${' '.repeat(depth * 4)}}`
   }
@@ -25,7 +25,7 @@ const stylish = (diff, depth = 1) => {
       case 'updated':
         return [
           `${indent(depth)}- ${node.key}: ${stringify(node.oldValue, depth)}`,
-          `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`
+          `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`,
         ].join('\n')
       case 'nested':
         return `${indent(depth)}  ${node.key}: {\n${stylish(node.children, depth + 1)}\n${indent(depth)}  }`
