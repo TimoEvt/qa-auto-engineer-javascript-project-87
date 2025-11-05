ec0f702b77096237b9d59713b0f0d5c5093cfbdc
TimoEvt <t.evteev@school1387.ru>
2025-11-05 12:03:59 +0400

    Refactor formatter exports to use getFormatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,14 @@
-import stylish from './stylish.js'
-import plain from './plain.js'
-import json from './json.js'
+import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
 
-export default {
-  stylish,
-  plain,
-  json
-}
+const formatters = { stylish, plain, json };
+
+const getFormatter = (formatName) => {
+  if (!formatters[formatName]) {
+    throw new Error(`Unknown format: ${formatName}`);
+  }
+  return formatters[formatName];
+};
+
+export default getFormatter;
