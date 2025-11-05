0e322a4c573d54050b6708c026d5ab421cc863e4
TimoEvt <t.evteev@school1387.ru>
2025-11-05 12:52:19 +0400

    Fix quotes and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-import js from "@eslint/js"
-import globals from "globals"
-import { defineConfig } from "eslint/config"
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"], // убрал jsx, если нет React
+    files: ['**/*.{js,mjs,cjs}'], // убрал jsx, если нет React
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node,
-        process: "readonly",
-        __dirname: "readonly",
-        test: "readonly",   // добавляем Jest
-        expect: "readonly", // добавляем Jest
+        process: 'readonly',
+        __dirname: 'readonly',
+        test: 'readonly', // добавляем Jest
+        expect: 'readonly', // добавляем Jest
       },
     },
     rules: {},
