1935bc175623e761454244d415e9986ada233462
TimoEvt <t.evteev@school1387.ru>
2025-11-05 12:41:49 +0400

    Refactor test file for gendiffPlain format

--- __tests__/gendiffPlain.test.js
+++ __tests__/gendiffPlain.test.js
@@ -1,15 +1,15 @@
-import genDiff from '../src/genDiff.js';
-import path from 'path';
+import genDiff from '../src/genDiff.js'
+import path from 'path'
 
-const file1 = path.resolve('__tests__/fixtures/file1.yml');
-const file2 = path.resolve('__tests__/fixtures/file2.yml');
+const file1 = path.resolve('__tests__/fixtures/file1.yml')
+const file2 = path.resolve('__tests__/fixtures/file2.yml')
 
 test('gendiff plain format', () => {
-  const diff = genDiff(file1, file2, 'plain');
+  const diff = genDiff(file1, file2, 'plain')
   const expected = `
 Property 'follow' was removed
 Property 'proxy' was removed
 Property 'timeout' was updated. From 50 to 20
-Property 'verbose' was added with value: true`.trim();
-  expect(diff).toBe(expected);
-});
+Property 'verbose' was added with value: true`.trim()
+  expect(diff).toBe(expected)
+})
