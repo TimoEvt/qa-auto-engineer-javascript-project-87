a42887b2e6d00d5a37a5154214a0b8c187a86acc
TimoEvt <t.evteev@school1387.ru>
2025-11-05 12:27:33 +0400

    // src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,3 +1,5 @@
+// src/formatters/index.js
+
 import stylish from './stylish.js'
 import plain from './plain.js'
 import json from './json.js'
@@ -5,10 +7,11 @@ import json from './json.js'
 const formatters = { stylish, plain, json }
 
 const getFormatter = (formatName) => {
-  if (!formatters[formatName]) {
+  const formatter = formatters[formatName]
+  if (!formatter) {
     throw new Error(`Unknown format: ${formatName}`)
   }
-  return formatters[formatName]
+  return formatter
 }
 
 export default getFormatter
