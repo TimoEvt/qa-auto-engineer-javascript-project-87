523c5c423cefbfac792383cc00a2a6668c81041d
TimoEvt <t.evteev@school1387.ru>
2025-11-05 12:07:05 +0400

    src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,14 @@
-import stylish from './stylish.js';
-import plain from './plain.js';
-import json from './json.js';
+import stylish from './stylish.js'
+import plain from './plain.js'
+import json from './json.js'
 
-const formatters = { stylish, plain, json };
+const formatters = { stylish, plain, json }
 
 const getFormatter = (formatName) => {
   if (!formatters[formatName]) {
-    throw new Error(`Unknown format: ${formatName}`);
+    throw new Error(`Unknown format: ${formatName}`)
   }
-  return formatters[formatName];
-};
+  return formatters[formatName]
+}
 
-export default getFormatter;
+export default getFormatter
